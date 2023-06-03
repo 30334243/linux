58816ff0439c3ca9433186148f406651fde39978
bvs <fabish@bk.ru>
2023-06-03 23:10:34 +0300

    edit install vcpkg

--- vcpkg/install.sh
+++ vcpkg/install.sh
@@ -1,12 +1,7 @@
-cd $1
-echo -n "Enter path install: "
-read path
-if test -z $path
-then
-   path=$PWD
-fi
+cd ~
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
+./bootstrap-vcpkg.sh -disableMetrics
 # sudo update-alternatives --set vcpkg "/usr/bin/vcpkg/vcpkg"
 sudo update-alternatives --install /usr/bin/vcpkg vcpkg $PWD/vcpkg 10
 # sudo update-alternatives --remove-all vcpkg
